63b4f2bf5d544533e3c3df3863aaec8f39c916ae
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2024-08-30 12:02:04 +0900

    ファイルを編集 コンソールに文字が表示できるように設計

--- 20240830_kadai/Program.cs
+++ 20240830_kadai/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _20240830_kadai
 {
@@ -10,7 +11,9 @@ namespace _20240830_kadai
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("いらっしゃいませ！");
+            Console.ReadLine();
         }
     }
 }
+
