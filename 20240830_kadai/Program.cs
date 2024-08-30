7d086efdfa09bd185ef6dceefb5035a0d0759d7e
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2024-08-30 12:02:34 +0900

    ファイルを編集 商品を繰り返し選択できるように設計

--- 20240830_kadai/Program.cs
+++ 20240830_kadai/Program.cs
@@ -13,6 +13,31 @@ namespace _20240830_kadai
         {
             Console.WriteLine("いらっしゃいませ！");
             Console.ReadLine();
+
+            List<Item> itemList = new List<Item>() {
+            new Item(1, "ひのきのぼう", 8),
+            new Item(2, "こんぼう", 30),
+            new Item(3,"どうのつるぎ",120)
+        };
+
+            int money = 1000;
+            int number;
+            Console.WriteLine("いらっしゃいませ！");
+            Console.ReadLine();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("所持金:" + money);
+                Console.WriteLine("アイテム" + itemList[0].id + "." + itemList[0].name + ":" + itemList[0].price);
+                Console.WriteLine("アイテム" + itemList[1].id + "." + itemList[1].name + ":" + itemList[1].price);
+                Console.WriteLine("アイテム" + itemList[2].id + "." + itemList[2].name + ":" + itemList[2].price);
+
+                Console.WriteLine("0キーで終了します。");
+
+                int.TryParse(Console.ReadLine(), out number);
+
+            }while(number > 0);
         }
     }
 }
